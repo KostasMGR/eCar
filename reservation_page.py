1dc34e4e35fafbf956a27e17d77b682334ddc602
Trimmm21 <ice23390076@uniwa.gr>
2026-05-03 16:48:35 +0300

    filter bug fixxed

--- reservation_page.py
+++ reservation_page.py
@@ -469,7 +469,7 @@ class ReservationsWindow(QWidget):
             """)
         res = functions.GetReservationByCarID(car["car_id"],self.session_email)
         print(res)
-        price_label = QLabel(f"€{res['total_price']} <span style='color: #6b7788; font-size: 12px; font-weight: 500;'>/ day</span>")
+        price_label = QLabel(f"€{res['total_price']} <span style='color: #6b7788; font-size: 12px; font-weight: 500;'>/ total</span>")
         price_label.setStyleSheet("""
             color: #1d2736;
             font-size: 18px;
